43b379f4fc7bc513efebe03302f079a9d8726ad3
naki7 <joshie.cad@gmail.com>
2018-08-25 16:57:42 +0200

    Fuze of enemy with player.
    also addition of enemy being able to chase player and fov as a mesh in game.

---
+++ 3rd Year Game/Assets/Scripts/Dhevan's Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class CameraController : MonoBehaviour
+{
+
+	private InputDevice controller;
+
+	public Transform playerT;
+	public float followTime = 0.15f;
+	public float FollowSpeed = 4f;
+	public float xPosOffset = 0f, yPosOffset = 3.5f, zPosoffset = -10f;
+
+	private bool cameraFlipped = false;
+	private Vector3 camRotation;
+	public float camFlipSpeed = 6f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		controller = InputManager.ActiveDevice;
+		camRotation = new Vector3 (this.transform.eulerAngles.x, 0f, this.transform.eulerAngles.z);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Update Camera Position
+		Vector3 newPosition = new Vector3 (playerT.position.x + xPosOffset, playerT.position.y + yPosOffset, playerT.position.z + zPosoffset);
+		this.transform.position = Vector3.Slerp (transform.position, newPosition, FollowSpeed * Time.deltaTime);
+
+		//Update Camera Rotation
+		Quaternion rot = Quaternion.Euler (camRotation);
+		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rot, Time.deltaTime * camFlipSpeed);
+
+		if (Input.GetKeyDown(KeyCode.Q) || controller.Action4) {
+			FlipCamera ();
+			GameObject.Find ("Player").SendMessage ("InvertControls");
+		}
+
+	}
+
+	void FlipCamera ()
+	{
+
+		if (cameraFlipped == false) {
+			camRotation = new Vector3 (this.transform.eulerAngles.x, 180f, this.transform.eulerAngles.z);
+			cameraFlipped = true;
+
+		} else {
+			camRotation = new Vector3 (this.transform.eulerAngles.x, 0f, this.transform.eulerAngles.z);
+			cameraFlipped = false;
+		}
+		zPosoffset = zPosoffset * -1f;
+
+	}
+}
+
---
+++ 3rd Year Game/Assets/Scripts/Dhevan's Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class PlayerController : MonoBehaviour {
+
+	private InputDevice controller;
+
+	public float moveSpeed = 10f;
+	public float dashSpeedFactor = 30f;
+	public float crouchSpeed = 5f;
+
+	private bool dashing = false;
+	private float startDashingTime;
+	private bool crouching = false;
+
+	private float xInput, yInput, zInput;
+	private bool controlsInverted = false;
+
+	public float jumpStrength = 4f;
+	public float fallStrengthFactor = 4f;
+	private bool jumped = false;
+	public float jumpHeight = 1.5f;
+	private Vector3 jumpPos;
+
+	private Rigidbody playerRB;
+
+	// Use this for initialization
+	void Start () {
+		controller = InputManager.ActiveDevice;
+		playerRB = this.GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		checkMovement ();
+		checkActionButtons ();
+		checkTriggerButtons ();
+		playerRB.rotation = Quaternion.identity; //prevent player object from rotating
+
+		if (Time.time >= startDashingTime + 1f) {
+			dashing = false;
+			playerRB.velocity = Vector3.zero;
+		}
+	}
+
+	void checkMovement(){
+		xInput = controller.LeftStick.X;  
+		zInput = controller.LeftStick.Y;
+
+		//Invert Controsl for when camera flips
+		if (controlsInverted) {
+			xInput *= -1f;
+			zInput *= -1f;
+		}
+		Vector3 Movement = new Vector3(xInput, 0f, zInput) * Time.deltaTime * moveSpeed;
+		this.transform.Translate(Movement, Space.World); 
+	}
+
+	void checkActionButtons(){
+		//Check Jump Action
+		if ((controller.Action1 || Input.GetKey (KeyCode.Space)) && jumped == false) {
+			playerRB.AddForce (new Vector3 (0f, jumpStrength * 100f, 0f), ForceMode.Force);
+			jumped = true;
+			jumpPos = this.transform.position;
+		} 
+		//Add a fall force after the jump
+		else if(jumped == true) {
+			if (this.transform.position.y - jumpPos.y >= jumpHeight) {
+				playerRB.AddForce (new Vector3 (0f, -1f * (jumpStrength*50f/fallStrengthFactor), 0f), ForceMode.Force);
+			}
+		}
+			
+	}
+	void checkTriggerButtons(){
+		if (controller.RightTrigger == true && dashing == false) {
+			playerRB.AddForce (new Vector3 (xInput, 0f, zInput) * dashSpeedFactor, ForceMode.Force);
+			dashing = true;
+			startDashingTime = Time.time;
+			Debug.Log ("Dashed");
+		}
+
+	}
+
+	void InvertControls (){
+		if (controlsInverted == true) {
+			controlsInverted = false;
+		} else {
+			controlsInverted = true;
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "Platform") {
+			jumped = false;
+		}
+	}
+}

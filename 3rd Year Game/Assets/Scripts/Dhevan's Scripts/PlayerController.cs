ea51aa6bc54f9ac0c99d9e6e7c1ad463d3d34a0e
Dhevan Lau <1433596@students.wits.ac.za>
2018-09-10 11:31:47 +0200

    Revert "Removing weird text added to the scripts"
    This reverts commit af24e2601ec89e48627d5b208426f2e2fb2ceff1.

--- 3rd Year Game/Assets/Scripts/Dhevan's Scripts/PlayerController.cs
+++ 3rd Year Game/Assets/Scripts/Dhevan's Scripts/PlayerController.cs
@@ -38,14 +38,13 @@ public class PlayerController : MonoBehaviour {
 		checkTriggerButtons ();
 		playerRB.rotation = Quaternion.identity; //prevent player object from rotating
 
-
+<<<<<<< HEAD
 		if (Time.time >= startDashingTime + 1f && dashing == true) {
-
-			if (Time.time >= startDashingTime + 1f) {
-
-				dashing = false;
-				playerRB.velocity = Vector3.zero;
-			}
+=======
+		if (Time.time >= startDashingTime + 1f) {
+>>>>>>> origin/Josh
+			dashing = false;
+			playerRB.velocity = Vector3.zero;
 		}
 	}
 
